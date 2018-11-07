0102cc1ae5dffb63ae111359ba66bbac96ec8f9d
Olga Telezhnaya <o.telezhnaya@securitycode.ru>
2018-11-07 12:06:52 +0300

    smth

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from flask import Flask, make_response, redirect
+from flask import Flask, make_response, redirect, render_template
+from flask_script import Manager
+
 
 app = Flask(__name__)
+manager = Manager(app)
 
 
 @app.route('/yay/<smth>')
 def index(smth):
-    response = make_response('{} with cookie'.format(smth))
+    response = make_response(f'{smth} with cookie')
     response.set_cookie('my_cookie', '666')
     return response
 
@@ -20,5 +23,12 @@ def err():
     return 'error', 400
 
 
+@app.errorhandler(404)
+def not_found(a):
+    print(a)
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
+    # args: runserver -d -p 5001
