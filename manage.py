fb46fbd4a6eecf2008693bb920e4ba53ec3affa0
telezhnaya <olyatelezhnaya@gmail.com>
2018-11-23 09:33:49 +0300

    Fix requirements.txt

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ import os
 from app import create_app
 from flask_script import Manager
 
+# To upgrade requirements.txt, use pipreqs
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
